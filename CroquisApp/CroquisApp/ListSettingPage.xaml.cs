f31ec8ef2df21f68995f295ff55aa7b5e1cc96c6
naoyuki touma <hackinglimit@yahoo.co.jp>
2020-07-27 11:50:24 +0900

    test

---
+++ CroquisApp/CroquisApp/ListSettingPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CroquisApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListSettingPage : ContentPage
+    {
+        public ListSettingPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Create_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ListCreatePage(), true);
+        }
+
+        private async void Edit_Clicked(object sender, EventArgs e)
+        {
+            //await Navigation.PushAsync(new CroquiStart(), true);
+            await DisplayAlert("test", "メッセージ", "OK");
+        }
+
+        private async void Delete_Clicked(object sender, EventArgs e)
+        {
+            //await Navigation.PushAsync(new CroquiStart(), true);
+            await DisplayAlert("test", "メッセージ", "OK");
+        }
+    }
+}
---
+++ CroquisApp/CroquisApp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CroquisApp
+{
+
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Setting_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ListSettingPage(), true);
+        }
+
+        private async void CroquiStart_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CroquiStart(), true);
+        }
+    }
+}
